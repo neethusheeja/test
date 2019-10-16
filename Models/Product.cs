4f92e9d0f4dbe8f89f63d3270348a78dccb569d8
neethusheeja <neethusurendran48@gmail.com>
2019-10-15 21:28:06 -0500

    first commit

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Flybycart.Models
+{
+    public class Product
+    {
+        public int id {get; set;}
+        public string name {get; set;}
+        public double price {get; set;}
+        public double quantity{get;set;}
+        public string pictureUrl {get; set;}
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function(){
+    $("a.mobile").click(function(){
+      $(".sidebar").slideToggle('fast');
+    });
+    window.resize = function(event){
+      if($(window).width() > 320){
+        $(".sidebar").show();
+      }
+    };
+  });
+  
